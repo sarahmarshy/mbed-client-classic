d484a63abec7f7254827a0bab304330d547affe7
Simo Sillankorva <simo.sillankorva@arm.com>
2016-10-14 03:14:01 -0700

    Copyright headers fixed, removed unused files.

--- test/mbed-client-classic/unittest/common/alltests.cpp
+++ test/mbed-client-classic/unittest/common/alltests.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 ARM Limited. All rights reserved.
+ * Copyright (c) 2016 ARM Limited. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  * Licensed under the Apache License, Version 2.0 (the License); you may
  * not use this file except in compliance with the License.
@@ -22,5 +22,3 @@ int main(int ac, char** av)
 	return CommandLineTestRunner::RunAllTests(ac, av);
 }
 
-//#include "../m2mbase/m2mbasetest.h"
-#include "../m2mobject/m2mobjecttest.h"
